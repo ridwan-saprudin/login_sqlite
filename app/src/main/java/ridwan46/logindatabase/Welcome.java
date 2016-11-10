acafc8e13905a0937ac503353a5e17d06476236b
ridwan-saprudin <sahabatridwan2@gmail.com>
2016-11-10 22:56:13 +0700

    versi 1.0

---
+++ app/src/main/java/ridwan46/logindatabase/Welcome.java
@@ -0,0 +1,13 @@
+package ridwan46.logindatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
